0d17cc19b167da725bd6c3f49d4c559083427e32
FarooqAmin007 <139410252+FarooqAmin007@users.noreply.github.com>
2026-02-24 16:21:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
+
+print("===== OUTFITTERS TRACKER STARTED =====")
+
+URL = "https://outfitters.com.pk/collections/men-outerwear/products/quilted-puffer-jacket-with-hood-1?variant=44728895373503"
+
+PUSHBULLET_TOKEN = "o.LMvhCtQSfJMHLEXu1ghkK7NOxYpwHlyc"
+
+CHECK_INTERVAL = 600
+
+last_price = None
+
+
+# PUSH NOTIFICATION
+def send_push(title, message):
+
+    print("Sending notification...")
+
+    data = {
+        "type": "note",
+        "title": title,
+        "body": message
+    }
+
+    headers = {
+        "Access-Token": PUSHBULLET_TOKEN,
+        "Content-Type": "application/json"
+    }
+
+    requests.post(
+        "https://api.pushbullet.com/v2/pushes",
+        json=data,
+        headers=headers
+    )
+
+
+# GET PRICE
+def get_price():
+
+    print("Fetching price...")
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(URL, headers=headers)
+
+    html = r.text
+
+    # Find PKR price
+    match = re.search(r'PKR\s*([\d,]+)', html)
+
+    if match:
+        price = float(match.group(1).replace(",", ""))
+        return price
+
+    # Backup method
+    match2 = re.search(r'(\d{3,5})\.00', html)
+
+    if match2:
+        return float(match2.group(1))
+
+    return None
+
+
+# MAIN LOOP
+while True:
+
+    price = get_price()
+
+    if price:
+
+        print("Current Price:", price)
+
+        if last_price is None:
+
+            last_price = price
+
+            send_push(
+                "Tracker Started",
+                f"Monitoring price Rs {price}"
+            )
+
+        elif price != last_price:
+
+            send_push(
+                "🔥 Price Changed",
+                f"Old Price Rs {last_price}\nNew Price Rs {price}\n{URL}"
+            )
+
+            last_price = price
+
+    else:
+
+        print("Price not found")
+
+    time.sleep(CHECK_INTERVAL)
